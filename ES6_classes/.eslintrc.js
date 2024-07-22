ee217c912c72cced5b78aca3ba03c63dd0dddcdc
Josemendez <jjmendezrodriguez@yahoo.com>
2024-07-22 18:11:27 -0400

    fix task 0

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -1,16 +1,37 @@
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es2021: true,
-      node: true
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12
-    },
-    rules: {
-      'no-console': 'off'
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
     }
-  };
-  
+  ]
+};
