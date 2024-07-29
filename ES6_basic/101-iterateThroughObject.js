25f7e1fa5eb40e709e94572d20d60d5866796d04
Josemendez <jjmendezrodriguez@yahoo.com>
2024-07-29 12:26:38 -0400

    task 14

---
+++ ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,10 @@
+// iterateThroughObject.js
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+
+  return employees.join(' | ');
+}
