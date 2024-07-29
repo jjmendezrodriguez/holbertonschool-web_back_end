9ca8a1b68a95e35362d0141e94795dff24b24e90
Josemendez <jjmendezrodriguez@yahoo.com>
2024-07-29 12:23:34 -0400

    task 13

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+// createIteratorObject.js
+export default function createIteratorObject(report) {
+  function* employeeGenerator() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }
+
+  return employeeGenerator();
+}
