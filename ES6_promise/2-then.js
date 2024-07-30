291a54befe0f5b682f8e308167d7c08657ec45c8
Josemendez <jjmendezrodriguez@yahoo.com>
2024-07-30 09:29:26 -0400

    task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,10 @@
-export default function getResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    resolve('Success');
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  })
+    .catch(() => new Error());
 }
