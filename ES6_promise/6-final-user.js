8f4c6ca8464287fb3d3b2fb9538f8f222bb3fa34
Josemendez <jjmendezrodriguez@yahoo.com>
2024-07-30 11:18:17 -0400

    task 10 and fix 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,8 +6,16 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const uploadPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? [result.value] : [result.reason],
-    })));
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
+      return {
+        status: result.status,
+        value: result.reason,
+      };
+    }));
 }
