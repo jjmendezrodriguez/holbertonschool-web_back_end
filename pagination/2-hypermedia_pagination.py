b38b4cc986918cd6470b3b425d32678067877d5d
Josemendez <jjmendezrodriguez@yahoo.com>
2024-08-25 22:04:21 -0400

    fix pycostyle

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -53,16 +53,17 @@ class Server:
         dataset = self.dataset()
 
         return dataset[start:end]
-def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         """ethod that returns pagination metadata along with the dataset"""
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
 
         return {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": page + 1 if page < total_pages else None,
-            "prev_page": page - 1 if page > 1 else None,
-            "total_pages": total_pages
-        }
+                "page_size": len(data),
+                "page": page,
+                "data": data,
+                "next_page": page + 1 if page < total_pages else None,
+                "prev_page": page - 1 if page > 1 else None,
+                "total_pages": total_pages
+            }
