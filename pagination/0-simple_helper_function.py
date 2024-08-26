c6a95774d7bb1f1223250d5191334609aed250e4
Josemendez <jjmendezrodriguez@yahoo.com>
2024-08-25 21:38:18 -0400

    task 0 fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -16,5 +16,5 @@ def index_range(page: int, page_size: int) -> tuple:
                range of items to be retrieved.
     """
     start_index = (page - 1) * page_size
-    end_index = start_index * page_size
+    end_index = page * page_size
     return (start_index, end_index)
