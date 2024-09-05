3274262001a9b3185ef6ff926a5008a3368d0dfc
josemendez <jjmendezrodriguez@yahoo.com>
2024-09-05 00:11:15 -0400

    prueba de erro que se corrige solo

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,13 +12,26 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Function takes in 2 int arguments """
-    allDelays: List[float] = []
-    List_of_tasks: List[Any] = []
-    for i in range(n):
-        List_of_tasks.append(asyncio.create_task(wait_random(max_delay)))
-    for results in asyncio.as_completed(List_of_tasks):
-        completed = await results
-        allDelays.append(completed)
-    """ return the list of all the delays (float values) """
-    return allDelays
+    """
+    Spawn wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay time for wait_random.
+
+    Returns:
+        List[float]: A list of delays in ascending order.
+    """
+    delays = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
+    # We build a new list in ascending order manually
+    ordered_delays = []
+    while delays:
+        min_delay = min(delays)
+        ordered_delays.append(min_delay)
+        delays.remove(min_delay)
+
+    return ordered_delays
